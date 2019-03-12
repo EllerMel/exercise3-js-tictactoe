f4f25480655fa63436a0498a1fe2daf27b0594ea
M Eller <msmaedev@gmail.com>
2019-03-12 15:20:30 -0400

    Full Overhaul

--- web/js/main.js
+++ web/js/main.js
@@ -1,194 +1,238 @@
-// ==== UI variables ====
+function createBoard() {
+  //Make grid
+  //Each cube needs to be clickable
+  var str = "";
+  var obj = document.getElementById("gameBoard");
 
-const msgDiv = document.querySelector('.message');
+  for (var i = 0; i < 3; i++) {
+    str += "<div class='row' id='test_row" + i + "'>";
+    var str2 = "";
 
-const gameBoardDiv = document.querySelector('.gameboard');
-const cells = document.querySelectorAll('.cell');
-const cellsArr = Array.from(cells);
+    for (var j = 0; j < 3; j++) {
+      str2 += "<div class='col-xs-4' id='test_col'>";
+      var btnKey = String(i) + String(j);
 
-const btnStart = document.querySelector('.btn--start');
-const btnReset = document.querySelector('.btn--reset');
-const playerOneScore = document.querySelector('.player-one.score');
-const playerTwoScore = document.querySelector('.player-two.score');
+      str2 += "<button type='button' id='" + btnKey + "' class='btn btn-secondary color' onclick=whoseTurn('" + btnKey + "')> ! </button>";
+      str2 += "</div>";
+    }
 
-
-// ==== factory functions =====
-const gameBoard = (() => {
-  const display = [ "", "", "",
-                    "", "", "",
-                   "", "", "" ];
+    str += str2;
+    str += "</div>";
+  }
   
-  // display markers on the board
-  const init = function(){
-    
-        
-    for (let i = 0; i < cellsArr.length; i++){
-      cellsArr[i].innerText = "";
-     }
-
-    msgDiv.innerText = "X's turn";
-  };
-  
-  return {display, init};
-  
-})();
-
-// object for creating players
-const player = (marker) => {
-  let score = 0;
-  return { marker, score};
+  console.log(str);
+  obj.innerHTML = str;
 }
 
-// players 
-const playerOne = player("X");
-const playerTwo = player("O");
+var alreadyClicked = [];
+var count = 0;
+var arrWinner = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-// object to control flow of the game itself
-const gameController = (() => {
- 
-  // start with X marker => playerOne
-  let currentPlayer = playerOne;
-  
-  // add new marker on the board
-  const nextMove = ((e) => {
-    
-    let board = gameBoard.display;
-    let gameWon;
-      if (e.target.innerText === ""){
-        e.target.innerText = currentPlayer.marker;
-        // update gameBoard array
-        const cellNb = e.target.dataset.cell;
-        board[cellNb-1] = currentPlayer.marker;
-        
-        gameWon = checkForWin(board);
-        // check for win
-        if (gameWon === true){
-          
-          msgDiv.innerText = `${currentPlayer.marker} has won!`;
-          currentPlayer.score += 1;
+function whoseTurn(buttonSpot){
+  // count = 1; count++
+  // X is the first one (odds)
+  // Y is the second (evens)
+  // Array keeping track of used tiles
+  //Cuts off function to used based on array
+  //Insert HTML into header for whose turn it is
+  console.log(buttonSpot);
 
-          displayScore()
-          // remove event listener from gameboard
-          cellsArr.forEach(cell => cell.removeEventListener("click", gameController.nextMove));
-
-        } // check for draw
-        else if (gameWon === false &&  board.includes("") ===false ) {
-          msgDiv.innerText = "It's a tie! Play again!";
-           
-          cellsArr.forEach(cell => cell.removeEventListener("click", gameController.nextMove));
-        } 
-        // keep playing
-        else{
-          currentPlayer = changePlayer(currentPlayer);
-          msgDiv.innerText= `${currentPlayer.marker}'s turn.`;
-        }
-        
-      } else {
-        msgDiv.innerText = "Clicked already. Choose another tile"
-      }
-        
-  })
-  
-   // change player
-  function changePlayer(currentPlayer){
-    if (currentPlayer === playerOne){
-      currentPlayer = playerTwo
-    } else{
-      currentPlayer = playerOne
-    }
-    
-    return currentPlayer;
+  var obj = document.getElementById(buttonSpot);
+  console.log(count);
+  if ((count % 2 == 0) && (!alreadyClicked.includes(buttonSpot))) {
+    count++;
+    alreadyClicked.push(buttonSpot);
+    var newButton = assignIndex(buttonSpot);
+    obj.innerHTML = "X";
+    arrWinner[newButton] = 1;
+    document.getElementById("whoseTurn").innerHTML = "Now it's O's turn";
+    console.log(arrWinner);
+    checkWinner(arrWinner);
   }
-
-  // winnig conditions
-  function checkRow(a,b,c, board){
-    let winnigRow = false;
-    if (board[a] === board[b] && 
-        board[a] === board[c] &&
-        board[a] !== ""){
-          winnigRow = true;
-      }
-   
-    return winnigRow;
+  else if ((count % 2 == 1) && (!alreadyClicked.includes(buttonSpot))) {
+    count++;
+    alreadyClicked.push(buttonSpot);
+    var newButton = assignIndex(buttonSpot);
+    obj.innerHTML = "O";
+    arrWinner[newButton] = 2;  
+    document.getElementById("whoseTurn").innerHTML = "Now it's X's turn";
+    console.log(arrWinner);
+    checkWinner(arrWinner);
   }
-
-  function checkForWin(board){
-    // check rows
-    if (checkRow(0,1,2,board) === true){
-      return true
-    }
-    else if (checkRow(3,4,5,board)){
-      return true
-    }
-    else if (checkRow(6,7,8,board)){
-      return true
-    }
-    // check columns
-    else if (checkRow(0,3,6,board)){
-      return true
-    }
-    else if (checkRow(1,4,7,board)){
-      return true
-    }
-    else if (checkRow(2,5,8,board)){
-      return true
-    }
-    // check diagonals
-    else if (checkRow(0,4,8,board)){
-      return true
-    }
-    else if (checkRow(2,4,6,board)){
-      return true
-    } // keep playing
-    else {
-      return false
-    }
-
-  }
-  function displayScore(){
-    playerOneScore.innerText = playerOne.score;
-    playerTwoScore.innerText = playerTwo.score;
-  }
-   return {nextMove}
-})();
-
-
-// ==== event listeners =====
-btnReset.addEventListener("click", function(){
-  //reset gameboard
-  gameBoard.display = [ "", "", "",
-  "", "", "",
- "", "", "" ];
-  gameBoard.init();
-  cellsArr.forEach(cell => cell.addEventListener("click", gameController.nextMove));
-});
-
-btnStart.addEventListener("click", function(){
-  gameBoardDiv.style.display = "grid";
-  gameBoard.init();
-  btnStart.style.display = "none";
-  btnReset.style.display = "block";
-  cellsArr.forEach(cell => cell.addEventListener("click", gameController.nextMove));
-})
-
-//Insert Player1 name and update page. Not required.
-function person1() {
-    var person1 = prompt("Please enter player one's name", "");
-    if (person1 != null) 
-    {
-        document.getElementById("PlayerOne").innerHTML =
-        "Player One" + " " + person1 + ": X";
-    }
 }
 
-//Insert Player2 name and update page. Not required.
-function person2() {
-    var person2 = prompt("Please enter player two's name", "");
-    if (person2 != null) 
-    {
-        document.getElementById("PlayerTwo").innerHTML =
-        "Player Two" + " " + person2 + ": O";
-    }
+function assignIndex(button){
+  if (button == "00") {
+    let newButton = 0;
+    console.log(newButton);
+    return newButton;
+  }
+  if (button == "01") {
+    let newButton = 1;
+    console.log(newButton);
+    return newButton;
+  }
+  if (button == "02") {
+    let newButton = 2;
+    console.log(newButton);
+    return newButton;
+  }
+  if (button == "10") {
+    let newButton = 3;
+    console.log(newButton);
+    return newButton;
+  }
+  if (button == "11") {
+    let newButton = 4;
+    console.log(newButton);
+    return newButton;
+  }
+  if (button == "12") {
+    let newButton = 5;
+    console.log(newButton);
+    return newButton;
+  }
+  if (button == "20") {
+    let newButton = 6;
+    console.log(newButton);
+    return newButton;
+  }
+  if (button == "21") {
+    let newButton = 7;
+    console.log(newButton);
+    return newButton;
+  }
+  if (button == "22") {
+    let newButton = 8;
+    console.log(newButton);
+    return newButton;
+  }
+
 }
 
-gameController.displayScore;
+function checkWinner(a) {
+  var winner = document.getElementById("WinnerMsg");
+
+  //Check rows
+  if ((a[0] + a[1] + a[2] == 3) && (a[0] != 0 && a[1] != 0 && a[2] != 0)) {
+    console.log("Winner is X!")
+    winner.innerHTML = "Winner is X!";
+    document.getElementById('gameBoard').style.display = "none";
+    document.getElementById("whoseTurn").innerHTML = "";
+
+  }
+  if ((a[0] + a[1] + a[2] == 6) && (a[0] != 0 && a[1] != 0 && a[2] != 0)) {
+    console.log("Winner is O!")
+    winner.innerHTML = "Winner is O!";
+    document.getElementById('gameBoard').style.display = "none";
+    document.getElementById("whoseTurn").innerHTML = "";
+  }
+
+  if ((a[3] + a[4] + a[5] == 3) && (a[3] != 0 && a[4] != 0 && a[5] != 0)) {
+    console.log("Winner is X!")
+    winner.innerHTML = "Winner is X!";
+    document.getElementById('gameBoard').style.display = "none";
+    document.getElementById("whoseTurn").innerHTML = "";
+  }
+  if ((a[3] + a[4] + a[5] == 6) && (a[3] != 0 && a[4] != 0 && a[5] != 0)) {
+    console.log("Winner is O!")
+    winner.innerHTML = "Winner is O!";
+    document.getElementById('gameBoard').style.display = "none";
+    document.getElementById("whoseTurn").innerHTML = "";
+  }
+
+  if ((a[6] + a[7] + a[8] == 3) && (a[6] != 0 && a[7] != 0 && a[8] != 0)) {
+    console.log("Winner is X!")
+    winner.innerHTML = "Winner is X!";
+    document.getElementById('gameBoard').style.display = "none";
+    document.getElementById("whoseTurn").innerHTML = "";
+  }
+  if ((a[6] + a[7] + a[8] == 6) && (a[6] != 0 && a[7] != 0 && a[8] != 0)) {
+    console.log("Winner is O!")
+    winner.innerHTML = "Winner is O!";
+    document.getElementById('gameBoard').style.display = "none";
+    document.getElementById("whoseTurn").innerHTML = "";
+    document.getElementById("whoseTurn").innerHTML = "";
+  }
+
+  //Check columns
+  if ((a[0] + a[3] + a[6] == 3) && (a[0] != 0 && a[3] != 0 && a[6] != 0)) {
+    console.log("Winner is X!")
+    winner.innerHTML = "Winner is X!";
+    document.getElementById('gameBoard').style.display = "none";
+    document.getElementById("whoseTurn").innerHTML = "";
+  }
+  if ((a[0] + a[3] + a[6] == 6) && (a[0] != 0 && a[3] != 0 && a[6] != 0)) {
+    console.log("Winner is O!")
+    winner.innerHTML = "Winner is O!";
+    document.getElementById('gameBoard').style.display = "none";
+    document.getElementById("whoseTurn").innerHTML = "";
+  }
+
+  if ((a[1] + a[4] + a[7] == 3) && (a[1] != 0 && a[4] != 0 && a[7] != 0)) {
+    console.log("Winner is X!")
+    winner.innerHTML = "Winner is X!";
+    document.getElementById('gameBoard').style.display = "none";
+    document.getElementById("whoseTurn").innerHTML = "";
+  }
+  if ((a[1] + a[4] + a[7] == 6) && (a[1] != 0 && a[4] != 0 && a[7] != 0)) {
+    console.log("Winner is O!")
+    winner.innerHTML = "Winner is O!";
+    document.getElementById('gameBoard').style.display = "none";
+    document.getElementById("whoseTurn").innerHTML = "";
+  }
+
+  if ((a[2] + a[5] + a[8] == 3) && (a[2] != 0 && a[5] != 0 && a[8] != 0)) {
+    console.log("Winner is X!")
+    winner.innerHTML = "Winner is X!";
+    document.getElementById('gameBoard').style.display = "none";
+    document.getElementById("whoseTurn").innerHTML = "";
+  }
+  if ((a[2] + a[5] + a[8] == 6) && (a[2] != 0 && a[5] != 0 && a[8] != 0)) {
+    console.log("Winner is O!")
+    winner.innerHTML = "Winner is O!";
+    document.getElementById('gameBoard').style.display = "none";
+    document.getElementById("whoseTurn").innerHTML = "";
+  }
+
+  //Check diagonals
+  if ((a[0] + a[4] + a[8] == 3) && (a[0] != 0 && a[4] != 0 && a[8] != 0)) {
+    console.log("Winner is X!")
+    winner.innerHTML = "Winner is X!";
+    document.getElementById('gameBoard').style.display = "none";
+    document.getElementById("whoseTurn").innerHTML = "";
+  }
+  if ((a[0] + a[4] + a[8] == 6) && (a[0] != 0 && a[4] != 0 && a[8] != 0)) {
+    console.log("Winner is O!")
+    winner.innerHTML = "Winner is O!";
+    document.getElementById('gameBoard').style.display = "none";
+    document.getElementById("whoseTurn").innerHTML = "";
+  }
+
+  if ((a[2] + a[4] + a[6] == 3) && (a[2] != 0 && a[4] != 0 && a[6] != 0)) {
+    console.log("Winner is X!")
+    winner.innerHTML = "Winner is X!";
+    document.getElementById('gameBoard').style.display = "none";
+    document.getElementById("whoseTurn").innerHTML = "";
+  }
+  if ((a[2] + a[4] + a[6] == 6) && (a[2] != 0 && a[4] != 0 && a[6] != 0)) {
+    console.log("Winner is O!")
+    winner.innerHTML = "Winner is O!";
+    document.getElementById('gameBoard').style.display = "none";
+    document.getElementById("whoseTurn").innerHTML = "";
+  }
+
+  if (a[0] != 0 && a[1] != 0 && a[2] != 0 && a[3] != 0 && a[4] != 0 && a[5] != 0 && a[6] != 0 && a[7] != 0 && a[8] != 0) {
+    winner.innerHTML = "Draw! Please Play Again!";
+    document.getElementById('gameBoard').style.display = "none";
+    document.getElementById("whoseTurn").innerHTML = "";
+  }
+}
+
+
+function start(){
+  console.log('At Start');
+
+  createBoard();
+
+}
